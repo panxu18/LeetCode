44a75c4767f4ba48087695c219b2b10eb3365c37
panxu18 <49966697+panxu18@users.noreply.github.com>
2020-08-23 14:57:56 +0800

    add aiqiyi0823

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import java.io.*;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.printf("now: %d\n", now.getTime());
-        Date copy = (Date) now.clone();
-        copy.setDate(10);
-        System.out.printf("now: %d\n", now.getTime());
-        System.out.printf("copy: %d\n", copy.getTime());
+
+
     }
 }
---
+++ src/xp/pan/aiqiyi/Test0823_1.java
@@ -0,0 +1,9 @@
+package xp.pan.aiqiyi;
+
+import java.util.Scanner;
+
+public class Test0823_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
