2463c5599e2009aa7920a12cda6639c32369d862
peng <hello@pb.com>
2020-09-06 21:25:32 +0800

    add tenxun

--- src/xp/PB/tengxun/Test2.java
+++ src/xp/PB/tengxun/Test2.java
@@ -4,46 +4,6 @@ package xp.PB.tengxun;
 import java.util.Scanner;
 
 public class Test2 {
-    private static int N, M;
-    private static Edge[] head = new Edge[100010];
-    private static boolean visited[] = new boolean[100010];
-
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        N = in.nextInt();
-        M = in.nextInt();
-        for (int i = 0; i < M; i++) {
-            int x = in.nextInt();
-            int pre = in.nextInt();
-            for (int j = 1; j < x; j++) {
-                int cur = in.nextInt();
-                addEdges(pre, cur);
-                pre = cur;
-            }
-        }
-        // dfs
-        int ans = dfs(0);
-        System.out.println(ans);
-    }
-
-    private static int dfs(int u) {
-        if (visited[u]) {
-            return 0;
-        }
-        visited[u] = true;
-        int res = 1;
-        for (Edge e = head[u]; e != null; e = e.next) {
-            res += dfs(e.to);
-        }
-        return res;
-    }
-
-    private static void addEdges(int from, int to) {
-        head[from] = new Edge(from, to, head[from]);
-        head[to] = new Edge(to, from, head[to]);
-    }
-
     static class Edge {
         int from, to;
         Edge next;
@@ -53,5 +13,42 @@ public class Test2 {
             this.to = to;
             this.next = next;
         }
+
+        private static void addEdges(int from, int to) {
+            root[from] = new Edge(from, to, root[from]);
+            root[to] = new Edge(to, from, root[to]);
+        }
+    }
+
+    private static int n, m;
+    private static boolean visited[] = new boolean[100000];
+    private static Edge[] root = new Edge[100000];
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        m = scan.nextInt();
+        for (int i = 0; i < m; i++) {
+            int x = scan.nextInt();
+            int curr = scan.nextInt();
+            for (int j = 1; j < x; j++) {
+                int next = scan.nextInt();
+                Edge.addEdges(curr, next);
+                curr = next;
+            }
+        }
+        int res = deepFirstSerach(0);
+        System.out.println(res);
+    }
+    private static int deepFirstSerach(int u) {
+        if (visited[u]) {
+            return 0;
+        }
+        visited[u] = true;
+        int res = 1;
+        for (Edge e = root[u]; e != null; e = e.next) {
+            res += deepFirstSerach(e.to);
+        }
+        return res;
     }
 }
