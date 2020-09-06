44683817d1af170ec0b6be6003c7acc3c3ee597f
peng <hello@pb.com>
2020-09-06 18:59:03 +0800

    add test

---
+++ src/xp/PB/Hello.java
@@ -0,0 +1,11 @@
+package xp.PB;
+
+/**
+ * @auther Peng
+ * @date 2020/9/6 - 18:50
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+    }
+}
