5667a5615d20484629aa47c4cf52448de05d59ae
peng <hello@pb.com>
2020-09-06 19:13:04 +0800

    add hello5

--- src/xp/PB/Hello.java
+++ src/xp/PB/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
         System.out.println("Hello word!");
         System.out.println("Hello word!");
         System.out.println("Hello word!");
+        System.out.println("Hello word!");
     }
 }
