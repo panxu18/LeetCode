fbcae5afd91a3ba865f3ab1ced43f4be8a588623
xupan <xp545945@163.com>
2020-09-08 18:46:13 +0800

    add xiaomi 0908

---
+++ src/xp/pan/xiaomi/Test0908_1.java
@@ -0,0 +1,27 @@
+package xp.pan.xiaomi;
+
+import java.util.Scanner;
+
+public class Test0908_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] strs = in.nextLine().split(" ");
+        for (String str : strs) {
+            System.out.println(check(str));
+        }
+
+    }
+
+    private static int check(String str) {
+        if (str.length() < 8 || str.length() > 120) {
+            return 1;
+        }
+        if (!str.matches(".*\\d.*")
+                || !str.matches(".*[A-Z].*")
+                || !str.matches(".*[a-z].*")
+                || !str.matches(".*[^\\dA-Za-z].*")) {
+            return 2;
+        }
+        return 0;
+    }
+}
---
+++ src/xp/pan/xiaomi/Test0908_2.java
@@ -0,0 +1,16 @@
+package xp.pan.xiaomi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test0908_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        char[][] 
+        while ((str = in.readLine()) != null) {
+
+        }
+    }
+}
