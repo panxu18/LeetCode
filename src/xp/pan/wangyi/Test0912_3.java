9b778a273e22dd26a580f40bb0d4d66c49b5e25d
xupan <xp545945@163.com>
2020-09-12 15:39:27 +0800

    add wangyi 0912_3

---
+++ src/xp/pan/wangyi/Test0912_3.java
@@ -0,0 +1,30 @@
+package xp.pan.wangyi;
+
+import java.util.Scanner;
+
+public class Test0912_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int K = in.nextInt();
+        int[] father = new int[N];
+        for (int i = 1; i < N; i++) {
+            father[i] = in.nextInt();
+        }
+        int maxDepth = 0;
+        for (int i = 1; i < N; i++) {
+
+            int dep = 0;
+            int p = i;
+            do {
+                dep++;
+            } while ((p = father[p]) != 0);
+            maxDepth = Math.max(maxDepth, dep);
+        }
+        if (maxDepth >= K) {
+            System.out.println(K + 1);
+        } else {
+            System.out.println(Math.min(N, 1 + maxDepth + ((K - maxDepth) >> 1)));
+        }
+    }
+}
