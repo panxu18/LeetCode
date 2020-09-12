59c3fd331cee5520f5f85d3f125ad8baf8645daa
xupan <xp545945@163.com>
2020-09-12 16:29:15 +0800

    add wangyi 0912_4

---
+++ src/xp/pan/wangyi/Test0912_4.java
@@ -0,0 +1,67 @@
+package xp.pan.wangyi;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test0912_4 {
+
+    private static int MAXN = 20007;
+    private static Edge[] head = new Edge[MAXN];
+    private static boolean[] used = new boolean[MAXN];
+    private static int[] match = new int[MAXN];
+    private static int M, F;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        M = in.nextLine().split(" ").length;
+        F = in.nextLine().split(" ").length;
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int from = in.nextInt();
+            int to = in.nextInt();
+            addEdge(from, to);
+        }
+        System.out.println(binaryMatch());
+
+    }
+
+    private static int binaryMatch() {
+        Arrays.fill(match, -1);
+        int res = 0;
+        for (int i = 0; i <= M + F; i++) {
+            if (match[i] == -1) {
+                Arrays.fill(used, false);
+                if (dfs(i)) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static boolean dfs(int i) {
+        used[i] = true;
+        for (Edge e = head[i]; e != null; e = e.next) {
+            int w = match[e.to];
+            if (!used[w] && (w == -1 || dfs(w))) {
+                match[i] = e.to;
+                match[e.to] = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void addEdge(int from, int to) {
+        head[from] = new Edge(from, to, head[from]);
+    }
+
+    static class Edge{
+        int from, to;
+        Edge next;
+        public Edge(int from, int to, Edge next) {
+            this.from = from;
+            this.to = to;
+            this.next = next;
+        }
+    }
+}
