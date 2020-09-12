5fae6f960ad94a2bbc5a55a9754df8631388b41b
xupan <xp545945@163.com>
2020-09-12 14:21:27 +0800

    忽略Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
-import java.io.*;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        for (int i = 1000; i < 9999; i++) {
+            if (i * 9 == revers(i)) {
+                System.out.println(i);
+            }
+        }
+        int[] arr = {};
+        HashSet<Integer> set;
 
 
+
+
+
+    }
+
+    public static int revers(int i) {
+        int res = 0;
+        while (i > 0) {
+            res *= 10;
+            res += i %10;
+            i /= 10;
+        }
+        return res;
     }
 }
