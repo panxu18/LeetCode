cb1533da5756e2c51981accba503d3b2c00d2400
panxu18 <xp545945@163.com>
2020-09-13 20:18:53 +0800

    add 滴滴0913_1

--- src/xp/pan/didi/Test0913_1.java
+++ src/xp/pan/didi/Test0913_1.java
@@ -1,4 +1,21 @@
 package xp.pan.didi;
 
+import java.util.Scanner;
+
 public class Test0913_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        String str = in.nextLine();
+        int start = 0;
+        StringBuilder ans = new StringBuilder();
+        while (start < str.length()) {
+            StringBuilder sb = new StringBuilder(str.substring(start, Math.min(str.length(), start + n)));
+            ans.append(sb.reverse().toString());
+            start += n;
+        }
+        System.out.println(ans);
+
+    }
 }
