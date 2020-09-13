f67f8ad3bd88e9ebe69a56250670aac8a4816cf4
panxu18 <xp545945@163.com>
2020-09-13 21:20:19 +0800

    add 度小满0913_2

--- src/xp/pan/duxiaoman/Test0913_2.java
+++ src/xp/pan/duxiaoman/Test0913_2.java
@@ -5,6 +5,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+/**
+ * 陷阱迷宫
+ * 在迷宫中#表示陷阱，陷阱需要花费M步，1表示墙壁，墙壁无法穿过，计算从起点左上角到达终点右下角最少需要的步。
+ */
 public class Test0913_2 {
     private static String[][] map = new String[105][];
     private static int[][] dis = new int[105][105];
@@ -30,15 +34,15 @@ public class Test0913_2 {
         Queue<Node> que = new LinkedList<>();
         if ("0".equals(map[0][0])) {
             que.add(new Node(0, 0));
-            dis[0][0] = 1;
+            dis[0][0] = 0;
         }
         while (!que.isEmpty()) {
             Node cur = que.poll();
             for (int i = 0; i < 4; i++) {
                 int nx = cur.x + dir[i][0];
-                int ny = cur.y + dir[i][0];
+                int ny = cur.y + dir[i][1];
                 if (nx >= 0 && nx < N && ny >= 0 && ny < N && !"1".equals(map[nx][ny])) {
-                    int newDis = dis[cur.x][cur.y] + ("0".equals(map[nx][ny]) ? 1 : M);
+                    int newDis = dis[cur.x][cur.y] + ("0".equals(map[nx][ny]) ? 1 : M + 1);
                     if (newDis < dis[nx][ny]) {
                         que.add(new Node(nx, ny));
                         dis[nx][ny] = newDis;
