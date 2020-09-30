8fa3e74526b39904fcd948d7cfbf6cb687090785
peng <hello@pb.com>
2020-09-30 17:59:42 +0800

    add tenxun

--- src/xp/PB/laohu/test3.java
+++ src/xp/PB/laohu/test3.java
@@ -9,42 +9,35 @@ import java.util.Scanner;
  */
 public class test3 {
 
-    public static int n;
-    public static int[] dai;
-
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
-        n = scan.nextInt();
-        dai = new  int[n];
+        int n = scan.nextInt();
+        int[] dai = new  int[n];
         int add = 0;
         long mul = 1;
         for(int i = 0;i<n;i++){
             dai[i] = scan.nextInt();
         }
         Arrays.sort(dai);
-        int num = count(0,add,mul);
-        System.out.println(num);
-
+        int res = count(dai,0,0,1);
+        System.out.println(res);
     }
-
-    public static int count(int pos, int add, long mul){
-
+    public static int count(int[] nums,int n, long add, long mul){
         int num = 0;
-        for(int i=pos;i<n;i++){
-            add += dai[i];
-            mul *= dai[i];
+        for(int i=n;i<nums.length;i++){
+            add += nums[i];
+            mul *= nums[i];
             if(add>mul)
-                num+=1+count(i+1,add,mul);
-            else if(dai[i]==i)
-                num+=count(i+1,add,mul);
+                num+=1+count(nums,i+1,add,mul);
+            else if(nums[i]==1)
+                num+=count(nums,i+1,add,mul);
             else
                 break;
-            add-=dai[i];
-            mul/=dai[i];
-            while (dai[i+1]==dai[i] && i+1<n){
-                ++i;
+            add-=nums[i];
+            mul/=nums[i];
+            while (i+1<nums.length&&nums[i+1]==nums[i]  ){
+               i++;
             }
-
         }
         return num;
     }
