847446d5bdcab60283def83e34027900b83179d1
xupan <xp545945@163.com>
2020-10-10 20:20:00 +0800

    modify 便利蜂1010_2

--- src/xp/pan/bianlifeng/Test1010_2.java
+++ src/xp/pan/bianlifeng/Test1010_2.java
@@ -7,15 +7,15 @@ public class Test1010_2 {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int k = scan.nextInt();
-        int[][] dp = new int[k + 1][n + 1];
-        dp[0][0] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
         for (int j = 1; j <= k; j++) {
             for (int i = 0; i <= n; i++) {
                 if (i >= j) {
-                    dp[j][i] = dp[j - 1][i - 1] + dp[j][i - j];
+                    dp[i] += dp[i - j];
                 }
             }
         }
-        System.out.println(dp[k][n]);
+        System.out.println(dp[n]);
     }
 }
